c96d2c09319564989ddd5f38e38b9cf280ec65ca
Unknown <ericromangarcial@gmail.com>
2019-02-13 19:59:50 -0700

    comit

---
+++ TPDM_UNIDAD1_PRACTICA3_ERICGARCIA/app/src/main/java/com/example/tpdm_unidad1_practica3_ericgarcia/Main2Activity.java
@@ -0,0 +1,14 @@
+package com.example.tpdm_unidad1_practica3_ericgarcia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        this.setTitle("Clubs");
+    }
+}
---
+++ TPDM_UNIDAD1_PRACTICA3_ERICGARCIA/app/src/main/java/com/example/tpdm_unidad1_practica3_ericgarcia/Main3Activity3.java
@@ -0,0 +1,13 @@
+package com.example.tpdm_unidad1_practica3_ericgarcia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main3Activity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main33);
+    }
+}
